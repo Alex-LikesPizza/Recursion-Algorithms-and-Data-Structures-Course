6a1458821c5c97de55efb343d02b550a09445ed7
Alex <106477187+Alex-LikesPizza@users.noreply.github.com>
2023-12-23 13:37:31 +0200

    ecuatii

---
+++ Olympiad/r-11/caramizi/caramizi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+int main(){
+
+  ifstream fin("caramizi.in");
+  ofstream fout("caramizi.out");
+
+  
+
+  fin.close();
+  ofstream.close();
+
+  return 0;
+}
