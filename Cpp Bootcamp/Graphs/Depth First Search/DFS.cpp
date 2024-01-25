393d98b76e8a3edc5bc0be94592354da1b322251
Alex-LikesPizza <alexandru13321@gmail.com>
2024-01-25 18:03:06 +0200

    DFS algorithm

--- Cpp Bootcamp/Graphs/Depth First Search/DFS.cpp
+++ Cpp Bootcamp/Graphs/Depth First Search/DFS.cpp
@@ -1,16 +1,50 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+
+#define NMAX 100
 using namespace std;
 
 struct Node{
-  vector<int> neighbors;
+  bool explored = false;
+  vector<int> neighborsId;
+
+} nodes[NMAX];
+
+bool existsPath = false;
+void findPath(int nodeId, int goalId){
+  nodes[nodeId].explored = true;
+  if(nodeId == goalId) {
+    existsPath = true;
+    return;
+  }
+  if(existsPath) return;
+
+  for(int neighborId : nodes[nodeId].neighborsId){
+    if(!nodes[neighborId].explored){
+      cout << "been to node: " << neighborId << endl;
+      findPath(neighborId, goalId);
+      if(existsPath)return;
+    }
+  }
 
 }
 
 int main(){
+  ifstream fin ("DFS.in");
+  ofstream fout ("DFS.out");
 
+  int from, to; fin >> from >> to;
+  int nodeLink1, nodeLink2;
+  while(fin >> nodeLink1 >> nodeLink2){
+    nodes[nodeLink1].neighborsId.push_back(nodeLink2);
+  }
 
+  findPath(from, 0);
+  cout << existsPath;
+
+  fin.close();
+  fout.close();
 
   return 0;
 }
